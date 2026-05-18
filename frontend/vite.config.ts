52a00b2254f335b827fc972056056e5f5526a388
dageitnere <darja.geitnere@gmail.com>
2026-05-18 22:23:16 +0300

    pievienoju host + port vite.config.ts, izveidoju docker-compose.yml, pievienoju setup informāciju par dockeru

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://backend:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
